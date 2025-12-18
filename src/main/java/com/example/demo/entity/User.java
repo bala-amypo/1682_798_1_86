b7540bc320d850c1774ab83b12fceebce4cc2e16
bala-amypo <bala@amypo.in>
2025-12-18 04:59:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArsConstructor
+
+public class User{
+@Id 
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+private String name;
+private String email;
+private String password;
+private String role;
+
+}
