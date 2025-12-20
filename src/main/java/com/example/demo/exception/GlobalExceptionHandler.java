b656cbca034df951251a2c00abb165e147486ee7
bala-amypo <bala@amypo.in>
2025-12-20 15:51:34 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,21 +1,7 @@
-// package com.example.demo.exception;
-
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-// import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-// @RestControllerAdvice
-// public class GlobalExceptionHandler {
-
-//     @ExceptionHandler(ResourceNotFoundException.class)
-//     public String handle(ResourceNotFoundException ex) {
-//         return ex.getMessage();
-//     }
-// }
-
 package com.example.demo.exception;
 
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,15 +9,5 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
-
-    @ExceptionHandler({BadRequestException.class, IllegalArgumentException.class})
-    public ResponseEntity<String> handleBadRequest(RuntimeException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleOther(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 }
