4b7fe0b5391714a35be6e79d6ac1dbf99500f123
bala-amypo <bala@amypo.in>
2025-12-25 07:39:52 +0000

    User commit

--- src/main/java/com/example/demo/util/ValidationUtil.java
+++ src/main/java/com/example/demo/util/ValidationUtil.java
@@ -1,12 +1,12 @@
 package com.example.demo.util;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class ValidationUtil {
-    private static final List<String> SEASONS =
-            Arrays.asList("Kharif", "Rabi", "Summer");
-
+    private static final List<String> VALID_SEASONS = Arrays.asList("Kharif", "Rabi", "Zaid");
+    
     public static boolean validSeason(String season) {
-        return SEASONS.contains(season);
+        return VALID_SEASONS.contains(season);
     }
 }
