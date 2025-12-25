033aad57671d674c432e215c1a48ae86ee6e0d77
bala-amypo <bala@amypo.in>
2025-12-25 15:35:03 +0000

    User commit

--- src/main/java/com/example/demo/dto/FarmRequest.java
+++ src/main/java/com/example/demo/dto/FarmRequest.java
@@ -8,13 +8,15 @@ public class FarmRequest {
     private Double soilPH;
     private Double waterLevel;
     private String season;
-    
+    private Long ownerId; // Added to link farm to a user
+
     public FarmRequest() {}
-    
-    public FarmRequest(String name, Double soilPH, Double waterLevel, String season) {
+
+    public FarmRequest(String name, Double soilPH, Double waterLevel, String season, Long ownerId) {
         this.name = name;
         this.soilPH = soilPH;
         this.waterLevel = waterLevel;
         this.season = season;
+        this.ownerId = ownerId;
     }
 }
