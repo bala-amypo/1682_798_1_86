4b0ffaa915465c40ce259c350a6cd4e309c3b601
bala-amypo <bala@amypo.in>
2025-12-27 06:20:15 +0000

    User commit

--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -1,3 +1,3 @@
-package com.example.demo.exception; 
-public class BadRequestException extends RuntimeException { public 
-BadRequestException(String msg) { super(msg); } } 
+
+package com.example.demo.exception;
+public class BadRequestException extends RuntimeException { public BadRequestException(String msg) { super(msg); } }
--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,3 +1,3 @@
-package com.example.demo.exception; 
-public class ResourceNotFoundException extends RuntimeException { public 
-ResourceNotFoundException(String msg) { super(msg); } } 
+
+package com.example.demo.exception;
+public class ResourceNotFoundException extends RuntimeException { public ResourceNotFoundException(String msg) { super(msg); } }
