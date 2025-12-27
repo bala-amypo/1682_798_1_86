fd7cab69f9f23dd847676f7eff575788a715fbb6
bala-amypo <bala@amypo.in>
2025-12-27 10:15:33 +0000

    User commit

--- src/main/java/com/example/demo/util/ValidationUtil.java
+++ src/main/java/com/example/demo/util/ValidationUtil.java
@@ -1,15 +1,11 @@
+
 package com.example.demo.util;
-
-import org.springframework.stereotype.Component;
-
-@Component
 public class ValidationUtil {
-
-    public boolean validSeason(String season) {
+    public static boolean validSeason(String season) {
         if (season == null) return false;
-
-        return season.equalsIgnoreCase("Kharif")
-                || season.equalsIgnoreCase("Rabi")
-                || season.equalsIgnoreCase("Summer");
+        return season.equalsIgnoreCase("Kharif") || 
+               season.equalsIgnoreCase("Rabi") || 
+               season.equalsIgnoreCase("Summer");
     }
 }
+
