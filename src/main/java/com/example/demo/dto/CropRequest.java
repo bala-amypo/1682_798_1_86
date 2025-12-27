88406e7461ce281de8148f1c45079fe03b65f6be
bala-amypo <bala@amypo.in>
2025-12-27 01:58:26 +0000

    User commit

--- src/main/java/com/example/demo/dto/CropRequest.java
+++ src/main/java/com/example/demo/dto/CropRequest.java
@@ -1,14 +1,46 @@
 package com.example.demo.dto;
 
-import lombok.*;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class CropRequest {
-    private String name;
-    private Double suitablePHMin;
-    private Double suitablePHMax;
-    private Double requiredWater;
+
+    private String cropName;
     private String season;
+    private String soilType;
+    private Double area;
+
+    // No-args constructor
+    public CropRequest() {
+    }
+
+    // Getters & Setters
+    public String getCropName() {
+        return cropName;
+    }
+
+    public void setCropName(String cropName) {
+        this.cropName = cropName;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public String getSoilType() {
+        return soilType;
+    }
+
+    public void setSoilType(String soilType) {
+        this.soilType = soilType;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
 }
